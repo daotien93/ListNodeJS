dab88c6f21f208abd56bd2fa3f973a25eba1563b
Henrry Tien (Dao Tien) <58262966+DTien93@users.noreply.github.com>
2023-12-10 20:53:39 +0700

    Middleware Login expamle

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ const app = express()
 const port = 3000
 const router = require('./apiRoute.js')
 
+//Middleware
+const checkAdminRole = (req, res, next)=> {
+    res.user.role = 'admin'
+}
+
+const checkLogin = (req, res, next) => {
+    if (Login) {
+        req.user = user
+        next()
+    } else {
+        res.json('User cannot be logged in')
+    }
+}
+
+
 app.use('/api/v1', router)
+
+app.use('/api/login', checkLogin, checkAdminRole)
 
 app.listen(port, () => {
     console.log(`Listening port ${port}`)
