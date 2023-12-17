670196d784f9c2ca24438cc284efd1c809a0fc94
Henrry Tien (Dao Tien) <58262966+DTien93@users.noreply.github.com>
2023-12-17 22:23:00 +0700

    Code register login

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const router = require('./apiRoute.js')
 const bodyParser = require('body-parser')
+const AccountModel = require('./models/accounts')
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
 
-// parse application/json
-app.use(bodyParser.json())
+//Router
+app.post('/register', (req, res, next) => {
+    var username = req.body.username
+    var password = req.body.password
 
-app.use('/api/v1', router)
+    AccountModel.findOne({
+        username: username
+    })
+    .then(data => {
+        if (data) {
+            res.json('User already list user')
+        } else {
+            return AccountModel.create({
+                username: username,
+                password: password
+            })
+        }
+    })
+    .then(data => {
+        res.json('Create account successfully')
+    })
+    .catch(err => {
+        res.status(500).json('Create account failed')
+    }) 
+})
+
+//Login
+app.post('/login', (req, res, next) => {
+    var username = req.body.username
+    var password = req.body.password
+
+    AccountModel.findOne({
+        username: username,
+        password: password
+    })
+    .then(data => {
+        if (data) {
+            res.json('Login successfully')
+        } else {
+            res.status(500).json('Account not found')
+        }
+    }) 
+    .catch(err => {
+        res.status(500).json('Error server')
+    })
+})
 
 app.listen(port, () => {
-    console.log(`Listening port ${port}`)
+    console.log(`Server started on ${port}`)
 })
